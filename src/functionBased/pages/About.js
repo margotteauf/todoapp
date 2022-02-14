f8c2c274ba1d83fd98f36e7a9cbc4f8d966e20d9
margotteauf <margotteauf@gmail.com>
2022-02-14 15:37:39 +0100

    mon premier commit de todo app

---
+++ src/functionBased/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react"
+// import { useParams } from "react-router-dom";
+
+// const params = useParams;
+
+const About = (props) => {
+
+ return (
+  <div>
+    <ul>
+      page about
+    </ul>
+    </div>
+)
+}
+export default About
--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 //component file
-import TodoContainer from "./components/TodoContainer"
+import TodoContainer from "./functionBased/components/TodoContainer";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import About from "./functionBased/pages/About";
+import NotMatch from "./functionBased/pages/NotMatch";
+import Navbar from "./functionBased/components/Navbar";
+import SinglePage from "./functionBased/pages/SinglePage";
 
 //stylesheet
-import "./App.css"
+import "./functionBased/App.css";
+
+// const { path } = useLocation;
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TodoContainer />} />
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/about/:slug" element= { <SinglePage /> }/>
+        <Route path="*" element={<NotMatch />} />
+
+       
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
