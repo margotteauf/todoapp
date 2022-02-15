5c2fc8ffb493ad28c87b32f536e8172f5898d798
margotteauf <margotteauf@gmail.com>
2022-02-15 09:41:04 +0100

    Mise a jour lien et changement par Hashrouter

--- src/functionBased/pages/About.js
+++ src/functionBased/pages/About.js
@@ -1,16 +1,24 @@
 import React from "react"
 // import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom"
+
 
 // const params = useParams;
 
 const About = (props) => {
 
- return (
-  <div className="about__content">
-      <ul className="about__list">
-        page about
-    </ul>
-    </div>
-)
+    return (
+        <div className="about__content">
+          <ul className="about__list">
+            <li>
+              <Link to={`/about/about-app`}>About App</Link>
+            </li>
+            <li>
+              <Link to={`/about/about-author`}>About Author</Link>
+            </li>
+          </ul>
+      </div>
+    )
+
 }
 export default About
